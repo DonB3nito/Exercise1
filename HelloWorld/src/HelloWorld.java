9a78ff80a018c6c15096a7a5d6bffd602a5ae861
DonB3nito <david-villa2@web.de>
2015-10-27 17:35:58 +0100

    Change 2
    Hello again

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -12,7 +16,17 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.println("Hello ASE2015 - how are you now? :)");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Please enter your name: ");
+        try {
+            String s = br.readLine();
+            HelloUser user = new HelloUser(s);
+            user.greetUser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 	}
 
 }
